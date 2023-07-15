7a47cdf0bc1782b58d2161d6480b3730f88c7081
safal45 <safal.22545@knit.ac.in>
2023-07-15 19:49:04 +0530

    store application in database

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import email
 from sqlalchemy import create_engine,text
 import os
 
@@ -34,5 +35,17 @@ def load_job_from_db(id):
             return rows[0]._asdict()
 
 
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        
+        conn.execute(query, {
+            'job_id': job_id,
+            'full_name': data['full_name'],
+            'email': data['email'],
+            'linkedin_url': data['linkedin_url'],
+            'education': data['education'],
+            'work_experience': data['work_experience'],
+            'resume_url': data['resume_url']
+        })
 
-       
